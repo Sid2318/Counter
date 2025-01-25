8f41a39747166461655885f0e63f2a57ff332f0b
Sid2318 <moholsiddhi18@gmail.com>
2025-01-25 21:35:00 +0530

    25/1/25

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [count, setCount] = useState(0)
+
+let click1 = (e)=> {
+  setCount(count + 1)
+}
+
+let click2 = (e)=> {
+  setCount(count - 1)
+}
+let reload =(e) => {
+  window.location.reload()
+}
+
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>Counter</h1>
+      <div className="count-block">{count}</div>
+      <div>
+        <button onClick = {click1} onChange={(e) => setCount(e.target.value)} >+</button>
+        <button onClick = {click2} onChange={(e) => setCount(e.target.value)} >-</button>
+      </div>
+      <button onClick = {reload}> ↻ </button>
     </div>
   );
 }
